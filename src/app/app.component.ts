479f6293bd7f0b3bdeac67e1f54b08a98292000c
Sunny Yenpure <sunnyyenpure77@gmail.com>
2025-11-28 09:27:16 +0530

    ngcontent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Ipost } from './shared/models/post';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Ng-Content';
+postArr:Array<Ipost>=[]
+POST_URL:string='https://jsonplaceholder.typicode.com/posts'
+
+constructor(private _httpclient: HttpClient){
+       
+
+}
+ngOnInit(): void {
+  this._httpclient.get(this.POST_URL)
+  .subscribe((res:any)=>{
+    console.log(res);
+    this.postArr = res; 
+  })
+}
+onEdit(id:number){
+  console.log(id);
+  
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './shared/component/test/test.component';
+import { AuthComponent } from './shared/component/auth/auth.component';
+import {HttpClientModule} from "@angular/common/http"
+// import { HttpClientModule } from '@angular/common/http';
+// import { PostCardComponent } from './shared/component/post-card/post-card.component';
 
+// import {HttpClientModule} from '@angular/common/http';
+import { PostCardComponent } from './shared/component/post-card/post-card.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    AuthComponent,
+    PostCardComponent,
+    // PostCardComponent,
+    // PostCardComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, 
+    HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  
 })
-export class AppModule { }
+export class AppModule {
+  
+}
